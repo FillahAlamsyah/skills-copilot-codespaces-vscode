120d57a904d1129d7680ae4b35abf0e50bd27a94
Fillah Alamsyah <87614211+FillahAlamsyah@users.noreply.github.com>
2024-10-02 16:09:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,11 @@
-// Create Web Server
-
-// Import modules
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-// Set up the server
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
-
-// Create a new comment
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    const comments = fs.readFileSync('comments.json');
-    const commentsArray = JSON.parse(comments);
-    commentsArray.push(comment);
-
-    fs.writeFileSync('comments.json', JSON.stringify(commentsArray));
-
-    res.json(comment);
+// Create web server
+var express = require('express');
+var app = express();
+// Create a route for comments
+app.get('/comments', function(req, res){
+    res.send('This is the comments page');
 });
-
-// Get all comments
-app.get('/comments', (req, res) => {
-    const comments = fs.readFileSync('comments.json');
-    res.json(JSON.parse(comments));
-});
-
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Start the server on port 3000
+app.listen(3000, function(){
+    console.log('Server started on port 3000');
 });
